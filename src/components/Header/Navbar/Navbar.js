331ffd41956c513be8e40261ac69cc829e148289
bisaalz <bisaalz@gmail.com>
2022-04-13 14:26:57 +0545

    Navbar tested3

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -4,14 +4,41 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     
-<nav className='main-nav'>
-    <ul className='main-nav-ul'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contacts</li>
-    </ul>
-</nav>
+<>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Bishal</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
   )
 }
 
