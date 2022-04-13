6efe6dc1862992ea5e566667223afc824d629435
bisaalz <bisaalz@gmail.com>
2022-04-13 14:02:19 +0545

    Navbar added1

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -10,16 +10,16 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">About</a>
+        <a className="nav-link" href="">About</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Blog</a>
+        <a className="nav-link" href="">Blog</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
+        <a className="nav-link" href="">Contact</a>
       </li>
     </ul>
   </div>
